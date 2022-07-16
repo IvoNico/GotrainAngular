f3a33d8eeb5d3071f2bc2e99d1a72c28fe0b72c3
Ivo Nicolás Chapochnikoff <chapochnikoffivo@gmail.com>
2022-07-15 21:09:26 -0300

    Angular-Gotrain

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { StudentsComponent } from './components/students/students.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    StudentsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
